be99b56160eb0a3a2c9043a73a9c2ab93034ca63
Michael Rivnak <mrivnak86@gmail.com>
2020-03-23 16:05:00 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+cd $HOME
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+cp -r .assets .config .local .vim Pictures .vimrc .zshrc $HOME
