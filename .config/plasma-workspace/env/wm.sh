809dc2872ba647745bdaff9d2869c67511d1474c
Michael Rivnak <mrivnak86@gmail.com>
2020-03-24 17:24:55 -0400

    Disable i3 KDEWM override

--- .config/plasma-workspace/env/wm.sh
+++ .config/plasma-workspace/env/wm.sh
@@ -10,7 +10,7 @@
 # KDE window manager config file
 
 # Disable KWin and use i3gaps as WM
-export KDEWM=/usr/bin/i3
+# export KDEWM=/usr/bin/i3
 
 # Compositor (Animations, Shadows, Transparency)
 # xcompmgr -C
