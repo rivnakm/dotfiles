35d519d059402b76e8562156d9e1529b9e5f26f4
Michael Rivnak <rivnakm1@gmail.com>
2022-02-25 07:59:18 -0500

    Added konsole configs

--- scripts/install_sym.sh
+++ scripts/install_sym.sh
@@ -9,6 +9,10 @@ ln -sfv {dotfiles,$HOME}/.config/kitty/*
 ln -sfv {dotfiles,$HOME}/.config/nvim/init.vim
 ln -sfv {dotfiles,$HOME}/.config/polybar/*
 ln -sfv {dotfiles,$HOME}/.config/rofi/*
+ln -sfv {dotfiles,$HOME}/.config/konsolerc
+
+mkdir -pv $HOME/.local/share/konsole/
+ln -sfv {dotfiles,$HOME}/.local/share/konsole/*
 # mkdir -pv $HOME/.config/picom
 # ln -sfv {dotfiles,$HOME}/.config/picom/picom.conf
 
