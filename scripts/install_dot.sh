bf2ab28544f2a5b077fd2612487fe5f0e38b13ce
Michael Rivnak <rivnakm1@gmail.com>
2022-02-25 12:07:57 -0500

    More color schemes

--- scripts/install_dot.sh
+++ scripts/install_dot.sh
@@ -6,18 +6,15 @@ cp -v {dotfiles,$HOME}/.p10k.zsh
 mkdir -pv $HOME/.config/{bspwm,kitty,nvim,polybar,rofi,sxhkd}
 cp -v {dotfiles,$HOME}/.config/bspwm/bspwmrc
 cp -v {dotfiles,$HOME}/.config/kitty/kitty.conf
-cp -v {dotfiles,$HOME}/.config/kitty/nord.conf
+cp -v {dotfiles,$HOME}/.config/kitty/onedark.conf
 cp -v {dotfiles,$HOME}/.config/nvim/init.vim
 cp -v {dotfiles,$HOME}/.config/polybar/config
 cp -v {dotfiles,$HOME}/.config/polybar/launch.sh
 cp -v {dotfiles,$HOME}/.config/rofi/config.rasi
-cp -v {dotfiles,$HOME}/.config/rofi/nord.rasi
-cp -v {dotfiles,$HOME}/.config/konsolerc
+cp -v {dotfiles,$HOME}/.config/rofi/onedark.rasi
 
-mkdir -pv $HOME/.local/share/konsole/
-cp -v {dotfiles,$HOME}/.local/share/konsole/*
-# mkdir -pv $HOME/.config/picom
-# cp -v {dotfiles,$HOME}/.config/picom/picom.conf
+mkdir -pv $HOME/.config/picom
+cp -v {dotfiles,$HOME}/.config/picom/picom.conf
 
 mkdir -pv $HOME/Pictures
 cp -v {dotfiles,$HOME}/Pictures/wallpaper.jpg
