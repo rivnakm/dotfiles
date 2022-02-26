e60062f4aefb159bf929ece993a5652df749034e
Michael Rivnak <rivnakm1@gmail.com>
2022-02-25 20:10:15 -0500

    Removed extra mkdir

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir -pv .local/share/fonts/
 pushd /tmp
 git clone --depth=1 https://github.com/ryanoasis/nerd-fonts
 sudo bash ./nerd-fonts/install.sh --install-to-system-path FiraCode
