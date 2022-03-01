b367d12cf7f478077d511495c817eafdee7c24ee
Michael Rivnak <rivnakm1@gmail.com>
2022-02-28 22:22:52 -0500

    new fonts script

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pushd /tmp
-git clone --depth=1 https://github.com/ryanoasis/nerd-fonts
-sudo bash ./nerd-fonts/install.sh --install-to-system-path FiraCode
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O fonts.zip
+sudo mkdir -pv "/usr/share/fonts/Fira Code"
+sudo unzip fonts.zip -d "/usr/share/fonts/Fira Code"
 popd
