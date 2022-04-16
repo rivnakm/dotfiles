32097123319bb62500d3b600d37f59f701f8e863
Michael Rivnak <rivnakm1@gmail.com>
2022-04-16 02:04:38 -0400

    Added hack as fallback font

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 pushd /tmp
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O fonts.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O firacode.zip
 sudo mkdir -pv "/usr/share/fonts/Fira Code"
-sudo unzip fonts.zip -d "/usr/share/fonts/Fira Code"
+sudo unzip firacode.zip -d "/usr/share/fonts/Fira Code"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O hack.zip
+sudo mkdir -pv "/usr/share/fonts/Hack"
+sudo unzip hack.zip -d "/usr/share/fonts/Hack"
 popd
