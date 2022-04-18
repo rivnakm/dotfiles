4c3c88a7a5a1e70cdb8658b57942df3421802945
Michael Rivnak <rivnakm1@gmail.com>
2022-04-17 23:23:49 -0400

    Added UbuntuMono font

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -7,4 +7,7 @@ sudo unzip firacode.zip -d "/usr/share/fonts/Fira Code"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O hack.zip
 sudo mkdir -pv "/usr/share/fonts/Hack"
 sudo unzip hack.zip -d "/usr/share/fonts/Hack"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip -O UbuntuMono.zip
+sudo mkdir -pv "/usr/share/fonts/UbuntuMono"
+sudo unzip UbuntuMono.zip -d "/usr/share/fonts/UbuntuMono"
 popd
