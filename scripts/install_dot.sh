5644fdbbd2f982ad293f4385ed80965743d0a091
Michael Rivnak <rivnakm1@gmail.com>
2022-04-19 22:32:18 -0400

    fix installation scripts

--- scripts/install_dot.sh
+++ scripts/install_dot.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-mkdir -pv $HOME/.config
-cp -vr $HOME/{dotfiles/,}.config/nvim
-cp -vr $HOME/{dotfiles/,}.config/fish
-cp -v $HOME/{dotfiles/,}.config/starship.toml
+mkdir -pv $HOME/.config/{fish,nvim}
+cp -vr dotfiles/.config/nvim/* $HOME/.config/nvim/
+cp -vr dotfiles/.config/fish/* $HOME/.config/fish/
+cp -v dotfiles/.config/starship.toml $HOME/.config
