93eca679c1d433f51e755eee09393e1901ef4a8c
Michael Rivnak <rivnakm1@gmail.com>
2022-05-11 00:12:36 -0400

    Install vim-plug

--- scripts/install_dot.sh
+++ scripts/install_dot.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -pv $HOME/.config/{fish,nvim}
-cp -vr dotfiles/.config/nvim/* $HOME/.config/nvim/
 cp -vr dotfiles/.config/fish/* $HOME/.config/fish/
 cp -v dotfiles/.config/starship.toml $HOME/.config
+
+git clone https://github.com/mrivnak/nvim-config $HOME/.config/nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
