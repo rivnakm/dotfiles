15f35f46ee4f83afde1a3c983593840863144495
Michael Rivnak <rivnakm1@gmail.com>
2022-08-12 13:43:00 -0400

    Add custom version of FiraCode with no ligatures

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 pushd /tmp
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O firacode.zip
-sudo mkdir -pv "/usr/share/fonts/Fira Code"
-sudo unzip firacode.zip -d "/usr/share/fonts/Fira Code"
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O firacode.zip
+#sudo mkdir -pv "/usr/share/fonts/Fira Code"
+#sudo unzip firacode.zip -d "/usr/share/fonts/Fira Code"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O hack.zip
 sudo mkdir -pv "/usr/share/fonts/Hack"
 sudo unzip hack.zip -d "/usr/share/fonts/Hack"
@@ -11,3 +11,4 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono
 sudo mkdir -pv "/usr/share/fonts/UbuntuMono"
 sudo unzip UbuntuMono.zip -d "/usr/share/fonts/UbuntuMono"
 popd
+cp -rv dotfiles/.local/share/fonts $HOME/.local/share/
