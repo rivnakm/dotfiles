4852907d1d37cf0adfa5239f439f85480ac6725d
Michael Rivnak <rivnakm1@gmail.com>
2022-10-24 20:12:39 -0400

    added bottom and tmux configs

--- scripts/install_dot.sh
+++ scripts/install_dot.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -pv $HOME/.config/{fish,helix/themes}
+mkdir -pv $HOME/.config/{fish,helix/themes,bottom,tmux}
 cp -vr dotfiles/.config/fish/* $HOME/.config/fish/
 cp -vr dotfiles/.config/helix/* $HOME/.config/helix/
+cp -vr dotfiles/.config/bottom/* $HOME/.config/bottom/
+cp -vr dotfiles/.config/tmux/* $HOME/.config/tmux
 cp -v dotfiles/.config/starship.toml $HOME/.config/
 
 wget https://raw.githubusercontent.com/mrivnak/github-helix-theme/main/github_dark.toml -O $HOME/.config/helix/themes/github_dark.toml
