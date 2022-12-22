7a30166dfc363032cbb209a770f2425c98cf03b2
Mike Rivnak <rivnakm1@gmail.com>
2022-12-22 01:53:53 -0500

    Get fonts directly

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-pushd /tmp
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O firacode.zip
-#sudo mkdir -pv "/usr/share/fonts/Fira Code"
-#sudo unzip firacode.zip -d "/usr/share/fonts/Fira Code"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O hack.zip
-sudo mkdir -pv "/usr/share/fonts/Hack"
-sudo unzip hack.zip -d "/usr/share/fonts/Hack"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip -O UbuntuMono.zip
-sudo mkdir -pv "/usr/share/fonts/UbuntuMono"
-sudo unzip UbuntuMono.zip -d "/usr/share/fonts/UbuntuMono"
-popd
-cp -rv dotfiles/.local/share/fonts $HOME/.local/share/
+mkdir $HOME/.local/share/fonts
+git clone https://github.com/mrivnak/CascadiaMono-Nerd-Font $HOME/.local/share/fonts/CascadiaMono-Nerd-Font
