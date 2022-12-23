65c188fc3420c8c8ff816bb3c275992eb1d8116b
Michael Rivnak <rivnakm1@gmail.com>
2022-12-23 15:34:32 -0500

    fix overwriting in powershell script

--- scripts/InstallDotfiles.ps1
+++ scripts/InstallDotfiles.ps1
@@ -3,10 +3,13 @@ $DotfilesPath = Split-Path $(Split-Path $ScriptPath -Parent) -Parent
 
 # Install files
 # Starship
-New-Item -Type Directory $HOME\.config
-Copy-Item $DotfilesPath\.config\starship.toml $HOME\.config\starship.toml
+New-Item -Type Directory -Force $HOME\.config | Out-Null
+Copy-Item -Force $DotfilesPath\.config\starship.toml $HOME\.config\starship.toml
 
 # Neovim
-git clone https://github.com/mrivnak/neovim-config $HOME\AppData\Local\nvim
-
-Copy-Item $DotfilesPath\Microsoft.PowerShell_profile.ps1 $PROFILE
+if (Test-Path -Path $HOME\AppData\Local\nvim) {
+	Write-Output "neovim config folder exists, skipping..."
+} else {
+	git clone https://github.com/mrivnak/neovim-config $HOME\AppData\Local\nvim
+}
+Copy-Item -Force $DotfilesPath\Microsoft.PowerShell_profile.ps1 $PROFILE
