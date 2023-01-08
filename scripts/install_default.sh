629b807f2a01a042a24b014377ff9e5eb142fb5f
Mike Rivnak <mrivnak@users.noreply.github.com>
2023-01-08 17:01:42 -0500

    fix default install script

--- scripts/install_default.sh
+++ scripts/install_default.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-$HOME/dotfiles/scripts/install_dot.sh
+$HOME/dotfiles/scripts/install_dotfiles.sh
 $HOME/dotfiles/scripts/install_fonts.sh
 $HOME/dotfiles/scripts/install_fetch.sh
 $HOME/dotfiles/scripts/install_kde.sh
