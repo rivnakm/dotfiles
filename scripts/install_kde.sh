a21f34ab54e1563a6a1803882699225f66dbfbc7
Mike Rivnak <rivnakm1@gmail.com>
2023-04-30 16:32:50 -0400

    add step to install kde colorscheme

--- scripts/install_kde.sh
+++ scripts/install_kde.sh
@@ -5,3 +5,6 @@ mkdir -pv $HOME/.local/share/{konsole,plasma-systemmonitor}
 cp -v dotfiles/.config/{konsolerc,systemmonitorrc} $HOME/.config/
 cp -vr dotfiles/.local/share/konsole/* $HOME/.local/share/konsole/
 cp -vr dotfiles/.local/share/plasma-systemmonitor/* $HOME/.local/share/plasma-systemmonitor/
+
+git clone https://github.com/catppuccin/kde /tmp/catppuccin-kde && cd /tmp/catppuccin-kde && ./install.sh
+rm -rf /tmp/catppuccin-kde
