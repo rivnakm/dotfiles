04eeb29c3d1a7f7e38aa3fb4fdeb6e056b958154
Mike Rivnak <rivnakm1@gmail.com>
2023-06-24 23:33:48 -0400

    install omz

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -28,6 +28,9 @@ else
     git clone https://github.com/mrivnak/neovim-config $HOME/.config/nvim
 fi
 
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
