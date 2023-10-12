186b202f3803ff465b88eac90f610fca8cb21b28
Mike Rivnak <rivnakm1@gmail.com>
2023-10-11 21:39:29 -0400

    remove plasma systemmonitor config

--- scripts/install_kde.sh
+++ scripts/install_kde.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -pv $HOME/.config
-mkdir -pv $HOME/.local/share/{konsole,plasma-systemmonitor}
-cp -v dotfiles/.config/{konsolerc,systemmonitorrc} $HOME/.config/
+mkdir -pv $HOME/.local/share/konsole
+cp -v dotfiles/.config/konsolerc $HOME/.config/
 cp -vr dotfiles/.local/share/konsole/* $HOME/.local/share/konsole/
-cp -vr dotfiles/.local/share/plasma-systemmonitor/* $HOME/.local/share/plasma-systemmonitor/
