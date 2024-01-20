69f4108e09c872afa110235f8e37cf1dc89a90a8
Mike Rivnak <mrivnak@users.noreply.github.com>
2024-01-20 14:42:09 -0500

    Update install_fetch.sh

--- scripts/install_fetch.sh
+++ scripts/install_fetch.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-for fetch in neofetch pfetch
-do
-    git clone https://github.com/dylanaraps/$fetch.git
-    pushd $fetch
-    sudo make install
-    popd
-    rm -rf $fetch
-done
+cargo install pfetch --locked
