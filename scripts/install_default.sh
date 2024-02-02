4603ad2e3a8a2ee649fe7fce287f248905e71f75
Mike Rivnak <rivnakm1@gmail.com>
2024-02-01 20:59:56 -0500

    check if script requirements are met

--- scripts/install_default.sh
+++ scripts/install_default.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+cmds=("git" "curl" "zsh" "cargo" "cmake" "make")
+
+echo "Checking script requirements"
+for cmd in "${cmds[@]}"; do
+    if command -v "$cmd" &> /dev/null; then
+        echo "$cmd found"
+    else
+        echo "Error: '$cmd' command not found."
+        exit 1
+    fi
+done
+
 $HOME/dotfiles/scripts/install_dotfiles.sh
 $HOME/dotfiles/scripts/install_fonts.sh
 $HOME/dotfiles/scripts/install_fetch.sh
