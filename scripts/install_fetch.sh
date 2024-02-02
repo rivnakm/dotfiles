0cfab6cce98e10cc1143af49573076f336a6608e
Mike Rivnak <rivnakm1@gmail.com>
2024-02-01 20:54:15 -0500

    update fetch install script

--- scripts/install_fetch.sh
+++ scripts/install_fetch.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
+# pfetch-rs
 cargo install pfetch --locked
+
+# fastfetch
+git clone https://github.com/fastfetch-cli/fastfetch
+cd fastfetch
+mkdir -p build
+cd build
+cmake ..
+cmake --build . --target fastfetch
+sudo cmake --install . --component fastfetch --prefix /usr/local
+cd ../..
+rm -rf fastfetch
