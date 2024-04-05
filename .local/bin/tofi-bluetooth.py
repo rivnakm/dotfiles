e13d52923db9422d08ec7d9866c58bbb29b60257
Mike Rivnak <rivnakm1@gmail.com>
2024-04-04 21:19:00 -0400

    remove pair option from tofi menu

--- .local/bin/tofi-bluetooth.py
+++ .local/bin/tofi-bluetooth.py
@@ -26,35 +26,18 @@ def disconnect():
     device_mac = pick_device("Connected")
     subprocess.run(["bluetoothctl", "disconnect", device_mac], check=True)
 
-def pair():
-    all_devices = subprocess.check_output(["bluetoothctl", "devices"]).decode().split("\n")
-    paired_devices = subprocess.check_output(["bluetoothctl", "devices"]).decode().split("\n")
-    unpaired_devices = [device for device in all_devices if device not in paired_devices]
-
-    device_dict = {}
-    for device in unpaired_devices:
-        _, device_mac, device_name = device.split(maxsplit=2)
-        device_dict[device_name] = device_mac
-
-    device_name = show_tofi(list(device_dict.keys()))
-    device_mac = device_dict[device_name]
-
-    subprocess.run(["bluetoothctl", "pair", device_mac], check=True)
-
 def unpair():
     device_mac = pick_device("Paired")
     subprocess.run(["bluetoothctl", "remove", device_mac], check=True)
 
 def main():
-    actions = ["connect", "disconnect", "pair", "unpair"]
+    actions = ["connect", "disconnect", "unpair"]
     action = show_tofi(actions)
     match action:
         case "connect":
             connect()
         case "disconnect":
             disconnect()
-        case "pair":
-            pair()
         case "unpair":
             unpair()
 
